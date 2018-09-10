fef1078e695f5cf4bad53476c68e476d816921d9
Tess <t.eleanor.o@gmail.com>
2018-09-10 14:29:22 -0500

    Add project files.

---
+++ to_algroithmsFirstAssignment/JavaScript.js
@@ -0,0 +1,14 @@
+﻿function main()
+{
+    var canvas = document.getElementById('stuff');
+    if (!canvas)
+    {
+        console.log("Doesn't contain element <canvas>");
+        return;
+    }
+
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgba(255,0,5,1.0)";
+    ctx.fillRect(120, 10, 150, 150);
+}
